0a7449abe9bd7b40fff7d25b149b89c80e8f7c09
Zeeshan Mughal <81948080+Batwings208@users.noreply.github.com>
2022-06-29 18:24:24 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+
+
+namespace mysqlconnection
+{
+    class MainFunction
+    {
+        static MySqlConnection connection;
+
+        public static void Connect(string user, string password, string database)
+        {
+            connection = new MySqlConnection();
+
+            try
+            {
+                connection.ConnectionString = "server = localhost; User Id = " + user + "; " +
+                    "Persist Security Info = True; database = " + database + "; Password = " + password;
+                connection.Open();
+                Console.WriteLine("Succesfully connected!");
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine("Not Successful! due to " + e.ToString());
+            }
+        }
+        public static void Main()
+        {
+            Connect("root", "root54321", "Car");
+            connection.Close();
+            Console.WriteLine($"Connection Confirmed: {connection}");
+        }
+    }
+}
